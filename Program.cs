cd1b022d800775756f3f8ebb6eaaedfbd9a707d0
unknown <nkosimova@gmail.com>
2020-04-10 10:20:20 +0500

    creating class DocumentWorker

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        class DocumentWorker
+        {
+            public virtual void OpenDocument()
+            {
+                Console.WriteLine("Документ открыт");
+            }
+             public virtual void EditDocument()
+            {
+                Console.WriteLine("Редактирование докуемнта доступно в версии Про");
+            }
+             public virtual void SaveDocument()
+            {
+                Console.WriteLine("Сохранение документа доступно в версии Про");
+            }
+        }
+
+
+    }
+}
